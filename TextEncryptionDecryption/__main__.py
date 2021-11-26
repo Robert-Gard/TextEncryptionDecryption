081d34de5df61745b8f3edc6a627f9b948ca2953
Robert Gardzielewski <robert.gardzielewski@mohawkcollege.ca>
2021-11-26 14:44:10 -0500

    fixed weird errors

--- TextEncryptionDecryption/__main__.py
+++ TextEncryptionDecryption/__main__.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+#<<<<<<< HEAD
 import base64
 import time
 import sys
@@ -11,7 +11,7 @@ def main():
 
 if __name__ == '__main__':
     main()
-=======
+
 import base64
 import time
 import sys
@@ -24,4 +24,4 @@ def main():
 
 if __name__ == '__main__':
     main()
->>>>>>> 86bc6f79ea239b9a3ae2f34a006358e598a0c9bf
+
