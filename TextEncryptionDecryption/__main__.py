86bc6f79ea239b9a3ae2f34a006358e598a0c9bf
Robert-Gard <90399626+Robert-Gard@users.noreply.github.com>
2021-11-26 13:56:48 -0500

    Add files via upload

---
+++ TextEncryptionDecryption/__main__.py
@@ -0,0 +1,12 @@
+import base64
+import time
+import sys
+import pythonPublishEncryption
+
+def main():
+    args = sys.argv[1:]
+    for arg in args:
+        print('passed argument :: {}'.format(arg))
+
+if __name__ == '__main__':
+    main()
